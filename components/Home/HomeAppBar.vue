103940c80a5d32644b835cbf8c0ff0a248766675
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2022-05-07 14:11:51 +0900

    スクロールイベントでツールバーの色を変化するように実装

--- components/Home/HomeAppBar.vue
+++ components/Home/HomeAppBar.vue
@@ -1,11 +1,22 @@
 <template>
-  <v-app-bar app dark>
-    <app-logo />
+  <v-app-bar
+    app
+    :dark="!isScrollPoint"
+    :height="homeAppBarHeight"
+    :color="toolbarStyle.color"
+    :elevation="toolbarStyle.elevation"
+  >
+    <app-logo @click.native="$vuetify.goTo('#scroll-top')" />
     <v-toolbar-title />
     <v-spacer />
 
     <v-toolbar-items class="ml-2">
-      <v-btn v-for="(menu, i) in menus" :key="`menu-btn-${i}`" text>
+      <v-btn
+        v-for="(menu, i) in menus"
+        :key="`menu-btn-${i}`"
+        text
+        @click="$vuetify.goTo(`#${menu.title}`)"
+      >
         {{ $t(`menus.${menu.title}`) }}
       </v-btn>
     </v-toolbar-items>
@@ -20,11 +31,38 @@ export default {
     menus: {
       type: Array,
       default: () => []
+    },
+    imgHeight: {
+      type: Number,
+      default: 0
     }
   },
-  data({ $config: { appName } }) {
+  data({ $config: { appName }, $store }) {
     return {
-      appName
+      appName,
+      scrollY: 0,
+      homeAppBarHeight: $store.state.styles.homeAppBarHeight
+    }
+  },
+  computed: {
+    isScrollPoint() {
+      return this.scrollY > this.imgHeight - this.homeAppBarHeight
+    },
+    toolbarStyle() {
+      const color = this.isScrollPoint ? 'white' : 'transparent'
+      const elevation = this.isScrollPoint ? 4 : 0
+      return { color, elevation }
+    }
+  },
+  mounted() {
+    window.addEventListener('scroll', this.onScroll)
+  },
+  beforeDestroy() {
+    window.removeEventListener('scroll', this.onScroll)
+  },
+  methods: {
+    onScroll() {
+      this.scrollY = window.scrollY
     }
   }
 }
---
+++ store/index.js
@@ -0,0 +1,14 @@
+export const state = () => ({
+  styles: {
+    homeAppBarHeight: 56
+  }
+})
+
+// 算出プロパティ
+export const getters = {}
+
+// stateの値を変更する場所
+export const mutations = {}
+
+// メソッド
+export const actions = {}
