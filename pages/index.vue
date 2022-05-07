1f3bb70164615dc5abcfabf0e0a9ce97fe8745c8
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2022-05-07 12:39:38 +0900

    アイキャッチ画像とアプリ名及びメニューの表示設定

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,40 @@
 <template>
   <v-app>
-    <home-app-bar />
+    <home-app-bar :menus="menus" />
+    <v-img
+      dark
+      src="https://picsum.photos/id/20/1920/1080?blur=5"
+      gradient="to top right, rgba(19,84,122,.6), rgba(128,208,199,.9)"
+      :height="imgHeight"
+    >
+      <v-row
+        align="center"
+        justify="center"
+        :style="{ height: `${imgHeight}px` }"
+      >
+        <v-col cols="12" class="text-center">
+          <h1 class="display-1 mb-4">
+            未来を作ろう。ワクワクしよう。
+          </h1>
+          <h4 class="subheading" :style="{ letterSpacing: '5px' }">
+            中小企業に特化した事業計画策定ツール
+          </h4>
+        </v-col>
+      </v-row>
+    </v-img>
     <v-sheet>
       <v-container fluid :style="{ maxWith: '1280px' }">
         <v-row v-for="(menu, i) in menus" :key="`menu-${i}`">
+          <v-col cols="12">
+            <v-card flat>
+              <v-card-title class="justify-center display-1">
+                {{ $t(`menus.${menu.title}`) }}
+              </v-card-title>
+              <v-card-text class="text-center">
+                {{ menu.subtitle }}
+              </v-card-text>
+            </v-card>
+          </v-col>
           <v-col cols="12">
             <div :is="`Home-${menu.title}`" />
           </v-col>
@@ -23,9 +54,18 @@ import HomeContact from '../components/Home/HomeContact.vue'
 import HomePrice from '../components/Home/HomePrice.vue'
 import HomeProducts from '../components/Home/HomeProducts.vue'
 export default {
-  components: { HomeAbout, HomeCompany, HomeContact, HomePrice, HomeProducts, HomeAppBar, AppFooter },
+  components: {
+    HomeAbout,
+    HomeCompany,
+    HomeContact,
+    HomePrice,
+    HomeProducts,
+    HomeAppBar,
+    AppFooter
+  },
   data() {
     return {
+      imgHeight: 500,
       menus: [
         {
           title: 'about',
