e5d00a74fd882c0804c4c928d132d75e09b8c26e
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2022-05-07 20:34:26 +0900

    レスポンシブデザイン対応

--- components/App/AppFooter.vue
+++ components/App/AppFooter.vue
@@ -1,11 +1,32 @@
 <template>
-  <div>
-    <v-footer absolute dark color="blank">
-      AppFooter.vue
+  <div :style="{ marginTop: `${height}px` }">
+    <v-footer absolute dark color="blank" :height="height">
+      <v-col cols="12" class="py-0">
+        <div class="text-center text-body-2">
+          &copy;{{ conpyRightYear }}
+          <strong>{{ appName }}</strong>
+        </div>
+      </v-col>
     </v-footer>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data({ $config: { appName } }) {
+    return {
+      appName,
+      height: 32
+    }
+  },
+  computed: {
+    conpyRightYear() {
+      const beinningYear = 2022
+      const thisYear = new Date().getFullYear()
+      return beinningYear < thisYear
+        ? `${beinningYear} - ${thisYear}`
+        : beinningYear
+    }
+  }
+}
 </script>
--- components/Home/HomeAppBar.vue
+++ components/Home/HomeAppBar.vue
@@ -7,26 +7,52 @@
     :elevation="toolbarStyle.elevation"
   >
     <app-logo @click.native="$vuetify.goTo('#scroll-top')" />
-    <v-toolbar-title />
+    <v-toolbar-title class="hidden-mobile-and-down">
+      {{ appName }}
+    </v-toolbar-title>
     <v-spacer />
 
-    <v-toolbar-items class="ml-2">
+    <v-toolbar-items class="ml-2 hidden-ipad-and-down">
       <v-btn
         v-for="(menu, i) in menus"
         :key="`menu-btn-${i}`"
+        :class="{ 'hidden-sm-and-down': menu.title === 'about' }"
         text
         @click="$vuetify.goTo(`#${menu.title}`)"
       >
         {{ $t(`menus.${menu.title}`) }}
       </v-btn>
     </v-toolbar-items>
+
+    <app-signup-button />
+    <app-login-button />
+
+    <v-menu bottom nudge-left="110" nudge-width="100">
+      <template #activator="{ on }">
+        <v-app-bar-nav-icon class="hidden-ipad-and-up" v-on="on" />
+      </template>
+      <v-list dense class="hidden-ipad-and-up">
+        <v-list-item
+          v-for="(menu, i) in menus"
+          :key="`menu-list-${i}`"
+          exact
+          @click="$vuetify.goTo(`#${menu.title}`)"
+        >
+          <v-list-item-title>
+            {{ $t(`menus.${menu.title}`) }}
+          </v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-menu>
   </v-app-bar>
 </template>
 
 <script>
+import AppLoginButton from '../App/AppLoginButton.vue'
 import AppLogo from '../App/AppLogo.vue'
+import AppSignupButton from '../App/AppSignupButton.vue'
 export default {
-  components: { AppLogo },
+  components: { AppLogo, AppSignupButton, AppLoginButton },
   props: {
     menus: {
       type: Array,
