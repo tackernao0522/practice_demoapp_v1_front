f5dd396f7a2e2f24859824ddc38f2c0a260e846c
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2022-05-08 20:58:04 +0900

    Vuetifyを使った会員登録フォームのバリデーション設定

--- pages/signup.vue
+++ pages/signup.vue
@@ -1,15 +1,23 @@
 <template>
   <user-form-card>
     <template #user-form-card-content>
-      <v-form v-model="isValid">
+      <!-- 編集 -->
+      <v-form ref="form" v-model="isValid">
         <user-form-name :name.sync="params.user.name" />
-        <user-form-email :email.sync="params.user.email" />
-        <user-form-password :password.sync="params.user.password" />
+        <user-form-email :email.sync="params.user.email" placeholder />
+        <user-form-password
+          :password.sync="params.user.password"
+          set-validation
+        />
+        <!-- disabled=true => ボタンクリックを無効にする -->
+        <!-- 編集 -->
         <v-btn
-          :disabled="!isValid"
+          :disabled="!isValid || loading"
+          :loading="loading"
           block
           color="appblue"
           class="white--text"
+          @click="signup"
         >
           登録する
         </v-btn>
@@ -23,14 +31,31 @@ import UserFormCard from '../components/User/UserFormCard.vue'
 import UserFormEmail from '../components/User/UserFormEmail.vue'
 import UserFormName from '../components/User/UserFormName.vue'
 import UserFormPassword from '../components/User/UserFormPassword.vue'
+
 export default {
-  components: { UserFormName, UserFormEmail, UserFormPassword, UserFormCard },
+  components: { UserFormCard, UserFormName, UserFormEmail, UserFormPassword },
   layout: 'before-login',
   data() {
     return {
       name: '',
       isValid: false,
+      loading: false,
       params: { user: { name: '', email: '', password: '' } }
+    }
+  },
+  methods: {
+    signup() {
+      this.loading = true
+      setTimeout(() => {
+        this.formReset()
+        this.loading = false
+      }, 1500)
+    },
+    formReset() {
+      this.$refs.form.reset()
+      for (const key in this.params.user) {
+        this.params.user[key] = ''
+      }
     }
   }
 }
