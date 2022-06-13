93470702874a2c72e29e53a164f4a235fe36d408
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2022-06-13 21:27:44 +0900

    65 動的なルートに表示する個別プロジェクトページを作成する

---
+++ layouts/project.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-app>
+    <logged-in-app-bar clipped-left>
+      <template #navigation-toggle-button>
+        <v-app-bar-nav-icon @click="drawer = !drawer" />
+      </template>
+    </logged-in-app-bar>
+
+    <project-navigation-drawer :drawer.sync="drawer" />
+    <v-main>
+      <Nuxt />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import LoggedInAppBar from '../components/loggedIn/LoggedInAppBar.vue'
+import ProjectNavigationDrawer from '../components/Project/ProjectNavigationDrawer.vue'
+export default {
+  components: { LoggedInAppBar, ProjectNavigationDrawer },
+  data() {
+    return {
+      drawer: null
+    }
+  }
+}
+</script>
