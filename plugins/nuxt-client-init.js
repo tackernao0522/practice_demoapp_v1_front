061f24bed07c8c2cde34bf9042dbaad00ae0d353
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2022-06-25 12:23:26 +0900

    90 リフレッシュトークンを使ってログイン状態を維持する

---
+++ plugins/nuxt-client-init.js
@@ -0,0 +1,9 @@
+export default async ({ $auth, $axios }) => {
+  await $axios
+    .post(
+      '/api/v1/auth_token/refresh',
+      {},
+      { validateStatus: status => $auth.resolveUnauthorized(status) }
+    )
+    .then(response => $auth.login(response))
+}
