fe92bf1d0bca835d0cb5b98ed5577ebf642af857
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2022-06-29 12:23:20 +0900

    96 エラーページをカスタマイズ

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,15 +1,19 @@
-export default ({ $axios, $auth }) => {
+export default ({ $axios, $auth, isDev }) => {
   $axios.onRequest((config) => {
     config.headers.common['X-Requested-With'] = 'XMLHttpRequest'
     if ($auth.token) {
       config.headers.common.Authorization = `Bearer ${$auth.token}`
     }
-    // eslint-disable-next-line no-console
-    console.log(config)
+    if (isDev) {
+      // eslint-disable-next-line no-console
+      console.log(config)
+    }
   })
   $axios.onResponse((config) => {
-    // eslint-disable-next-line no-console
-    console.log(config)
+    if (isDev) {
+      // eslint-disable-next-line no-console
+      console.log(config)
+    }
   })
   $axios.onError((e) => {
     // eslint-disable-next-line no-console
